375cc0a1d8eb78bf63144bdeeaa7d202af3bf56d
Siôn Le Roux <sinisterstuf@gmail.com>
2013-05-29 09:14:25 +0200

    Fix window icon and session list.
    The window icon didn't display. I guess zenity doesn't support using SVG
    for icons, so I changed to use the PNG instead.
    I also fixed the session list, which was supposed to be stripping the
    .vim file extension but was only doing it for the first one. I removed
    the $ anchor from sed's regular expression and now it works as intended.

--- prj-open-dialogue.sh
+++ prj-open-dialogue.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-prjdir=$HOME/.vim-sessions
+prjdir="$HOME/.vim-sessions"
 prjfiles=$(/bin/ls $prjdir | grep '\.vim$')
 prjfiles=$(echo $prjfiles | sed -s 's/ / FALSE /g')
 prjfiles=$(echo $prjfiles | sed -s 's/^/FALSE /')
-prjfiles=$(echo $prjfiles | sed 's/\.vim$//')
-selection=$(zenity --title="Prj-Open" --height="300"  --window-icon="/usr/share/pixmaps/vim.svg" --list --radiolist --column "Open" --column "Project" $prjfiles)
+prjfiles=$(echo $prjfiles | sed 's/\.vim//g')
+selection=$(zenity --title="Prj-Open" --height="300"  --window-icon="/usr/share/pixmaps/gvim.png" --list --radiolist --column "Open" --column "Project" $prjfiles)
 if [[ $selection == "" ]]
 then
     echo "no file selected; aborting!"
